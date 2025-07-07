1527c07ecf4925c3783aebc100444db5676fc8ab
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-07-07 10:05:15 -0400

    Enhance output formatting for Factory Method examples

--- DesignPatterns-Console/Program.cs
+++ DesignPatterns-Console/Program.cs
@@ -3,8 +3,9 @@ using DesignPatterns_Console.FactoryMethod.NotificationSystem.Client;
 using DesignPatterns_Console.FactoryMethod.VehicleSystem.Client;
 
 //FactoryMethod
+Console.WriteLine("===FACTORY METHOD PATTERN===");
 Notification.Run();
-Console.WriteLine();
+Console.WriteLine("------------------------------------");
 Vehicle.Run();
-Console.WriteLine();
+Console.WriteLine("------------------------------------");
 Document.Run();
