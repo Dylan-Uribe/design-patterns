8254b37f10b5bdc8a70173364c04be7657d4c810
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-07-07 11:12:22 -0400

    add singleton

--- DesignPatterns-Console/Program.cs
+++ DesignPatterns-Console/Program.cs
@@ -1,11 +1,15 @@
-﻿using DesignPatterns_Console.FactoryMethod.DocumentationSystem.Client;
+﻿//FactoryMethod
+using DesignPatterns_Console.FactoryMethod.DocumentationSystem.Client;
 using DesignPatterns_Console.FactoryMethod.NotificationSystem.Client;
 using DesignPatterns_Console.FactoryMethod.VehicleSystem.Client;
-
-//FactoryMethod
-Console.WriteLine("===FACTORY METHOD PATTERN===");
+/*Console.WriteLine("===FACTORY METHOD PATTERN===");
 Notification.Run();
 Console.WriteLine("------------------------------------");
 Vehicle.Run();
 Console.WriteLine("------------------------------------");
-Document.Run();
+Document.Run();*/
+
+//Singleton
+using DesignPatterns_Console.Singleton;
+Console.WriteLine("===SINGLETON PATTERN===");
+Client.Run();
---
+++ DesignPatterns-Console/Singleton/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns_Console.Singleton;
+
+public static class Client
+{
+    public static void Run()
+    {
+        var logger = Logger.Instance;
+        logger.Log("This is a log message.");
+        
+        var logger2 = Logger.Instance;
+        logger2.Log("This is another log message.");
+    }
+}
