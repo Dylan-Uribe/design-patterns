1f09332f99c4e30b226434415eafc36a5f16f6ec
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-07-14 00:07:06 -0400

    Add Factory folder

---
+++ Creational/DocumentSystem/Factory/DocumentCreator.cs
@@ -0,0 +1,8 @@
+﻿using Creational.DocumentSystem.Core;
+
+namespace Creational.DocumentSystem.Factory;
+
+public abstract class DocumentCreator
+{
+    public abstract IDocument CreateDocument(string title);
+}
---
+++ Creational/DocumentSystem/Factory/ReportCreator.cs
@@ -0,0 +1,11 @@
+﻿using Creational.DocumentSystem.Core;
+
+namespace Creational.DocumentSystem.Factory;
+
+public class ReportCreator : DocumentCreator
+{
+    public override IDocument CreateDocument(string title)
+    {
+        return new Report(title, "This is a default summary");
+    }
+}
