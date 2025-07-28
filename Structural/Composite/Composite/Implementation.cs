9e91d749790f65ae4876de0e32841086f037ebf5
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-07-28 01:17:56 -0600

    add composite example

---
+++ Structural/Composite/Composite/Implementation.cs
@@ -0,0 +1,62 @@
+﻿namespace Composite;
+
+/// <summary>
+/// Component interface
+/// </summary>
+public interface IFileSystemComponent
+{
+    void ShowDetails();
+    void Delete();
+}
+
+/// <summary>
+/// Leaf
+/// </summary>
+public class File(string name) : IFileSystemComponent
+{
+    public void ShowDetails()
+    {
+        Console.WriteLine($"File: {name}");
+    }
+
+    public void Delete()
+    {
+        Console.WriteLine($"Deleting File: {name}");
+    }
+}
+
+/// <summary>
+/// Composite class
+/// </summary>
+public class Folder(string name) : IFileSystemComponent
+{
+    private readonly List<IFileSystemComponent> _components = new List<IFileSystemComponent>();
+
+    public void AddComponent(IFileSystemComponent component)
+    {
+        _components.Add(component);
+    }
+    
+    public void RemoveComponent(IFileSystemComponent component)
+    {
+        _components.Remove(component);
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Folder: {name}");
+        foreach (var component in _components)
+        {
+            component.ShowDetails();
+        }
+    }
+    
+    public void Delete()
+    {
+        Console.WriteLine($"Deleting folder: {name}");
+        foreach (var component in _components)
+        {
+            component.Delete();
+        }
+    }
+}
---
+++ Structural/Composite/Composite/Program.cs
@@ -0,0 +1,21 @@
+﻿using Composite;
+using File = Composite.File;
+
+IFileSystemComponent file1 = new File("File1.txt");
+IFileSystemComponent file2 = new File("File2.txt");
+IFileSystemComponent file3 = new File("File3.txt");
+
+var folder1 = new Folder("Folder1");
+var folder2 = new Folder("Folder2");
+
+folder1.AddComponent(file1);
+folder1.AddComponent(file2);
+
+folder2.AddComponent(folder1);
+folder2.AddComponent(file3);
+
+Console.WriteLine("Folder structure:");
+folder2.ShowDetails();
+
+Console.WriteLine("\nDeleting folder2:");
+folder2.Delete();
