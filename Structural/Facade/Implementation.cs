a57978062f76b2002db362301b6ac225c246c6b1
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-07-28 00:27:53 -0600

    first facade

---
+++ Structural/Facade/Implementation.cs
@@ -0,0 +1,58 @@
+﻿namespace Facade;
+
+public class Facade()
+{
+    protected Subsystem1 _subsystem1;
+    protected Subsystem2 _subsystem2;
+
+    public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2) : this()
+    {
+        _subsystem1 = subsystem1;
+        _subsystem2 = subsystem2;
+    }
+
+    public string Operation()
+    {
+        var result = "Facade initializes subsystems:\n";
+        result += _subsystem1.Operation1();
+        result += _subsystem2.Operation2();
+        result += "Facade orders subsystems to perform the action:\n";
+        result += _subsystem1.Operation1();
+        result += _subsystem2.Operation2();
+        return result;
+    }
+}
+
+public class Subsystem1
+{
+    public string Operation1()
+    {
+        return "Subsystem1: Ready!\n";
+    }
+
+    public string Operation2()
+    {
+        return "Subsystem1: Ready!\n";
+    }
+}
+
+public class Subsystem2
+{
+    public string Operation1()
+    {
+        return "Subsystem2: Ready!\n";
+    }
+
+    public string Operation2()
+    {
+        return "Subsystem2: Ready!\n";
+    }
+}
+
+public class Client()
+{
+    public static void ClientCode(Facade facade)
+    {
+        Console.WriteLine(facade.Operation());
+    }
+}
---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,6 @@
+﻿using Facade;
+
+var subsystem1 = new Subsystem1();
+var subsystem2 = new Subsystem2();
+var facade = new Facade.Facade(subsystem1, subsystem2);
+Client.ClientCode(facade);
