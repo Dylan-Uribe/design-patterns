a6bb5caa723750cea2ffc30861b4e82dc43921a0
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-07-30 15:31:28 -0600

    add ChainOfResposibility pattern

---
+++ ChainOfResponsability/Implementation.cs
@@ -0,0 +1,87 @@
+﻿namespace ChainOfResponsability;
+
+public interface IHandler
+{
+    void SetNext(IHandler handler);
+    void HandleTicket(Ticket request);
+}
+
+public abstract class HandlerBase : IHandler
+{
+    private IHandler _nextHandler;
+
+    public void SetNext(IHandler handler)
+    {
+        _nextHandler = handler;
+    }
+
+    public virtual void HandleTicket(Ticket ticket)
+    {
+        if (CanhHandleTicket(ticket))
+        {
+            Handle(ticket);
+        }
+        else if (_nextHandler is not null)
+        {
+            _nextHandler.HandleTicket(ticket);
+        }
+        else
+        {
+            Console.WriteLine("Ticket cannot be handled");
+        }
+    }
+
+    protected abstract bool CanhHandleTicket(Ticket ticket);
+    protected abstract void Handle(Ticket ticket);
+}
+
+public class Level1SupportHandler : HandlerBase
+{
+    protected override bool CanhHandleTicket(Ticket ticket)
+    {
+        return ticket.Severity.Equals(Severity.Low);
+    }
+
+    protected override void Handle(Ticket ticket)
+    {
+        Console.WriteLine("Level 1 support handles the ticket");
+    }
+}
+
+public class Level2SupportHandler : HandlerBase
+{
+    protected override bool CanhHandleTicket(Ticket ticket)
+    {
+        return ticket.Severity.Equals(Severity.Medium);
+    }
+
+    protected override void Handle(Ticket ticket)
+    {
+        Console.WriteLine("Level 2 support handles the ticket");
+    }
+}
+
+public class Level3SupportHandler : HandlerBase
+{
+    protected override bool CanhHandleTicket(Ticket ticket)
+    {
+        return ticket.Severity.Equals(Severity.High);
+    }
+
+    protected override void Handle(Ticket ticket)
+    {
+        Console.WriteLine("Level 3 support handles the ticket");
+    }
+}
+
+public class Ticket
+{
+    public Severity Severity { get; set; }
+}
+
+public enum Severity
+{
+    Low,
+    Medium,
+    High
+}
---
+++ ChainOfResponsability/Program.cs
@@ -0,0 +1,16 @@
+﻿using ChainOfResponsability;
+
+var level3SupportHandler = new Level3SupportHandler();
+var level2SupportHandler = new Level2SupportHandler();
+var level1SupportHandler = new Level1SupportHandler();
+
+level1SupportHandler.SetNext(level2SupportHandler);
+level2SupportHandler.SetNext(level3SupportHandler);
+
+var ticket1 = new Ticket{Severity = Severity.Low};
+var ticket2 = new Ticket{Severity = Severity.Medium};
+var ticket3 = new Ticket{Severity = Severity.Medium};
+
+level1SupportHandler.HandleTicket(ticket1);
+level1SupportHandler.HandleTicket(ticket2);
+level1SupportHandler.HandleTicket(ticket3);
