927b5d8097e09484815a6b3c8e03fcd3a439cf2a
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-08-02 10:47:12 -0600

    add strategy pattern

---
+++ Strategy/Implementation.cs
@@ -0,0 +1,63 @@
+﻿namespace Strategy;
+
+/// <summary>
+/// strategy pattern implementation
+/// </summary>
+public interface IExportService
+{
+    void Export(Order order);
+}
+
+/// <summary>
+/// Concrete strategy.
+/// </summary>
+public class JsonExportService : IExportService
+{
+    public void Export(Order order)
+    {
+        Console.WriteLine($"Exporting {order.Name} to JSON");
+    }
+}
+
+/// <summary>
+/// Concrete strategy.
+/// </summary>
+public class XmlExportService : IExportService
+{
+    public void Export(Order order)
+    {
+        // Logic to export order as XML
+        Console.WriteLine($"Exporting {order.Name} to XML");
+    }
+}
+
+/// <summary>
+/// Concrete strategy.
+/// </summary>
+public class CsvExportService : IExportService
+{
+    public void Export(Order order)
+    {
+        Console.WriteLine($"Exporting {order.Name} to CSV");
+    }
+}
+
+public class Order
+{
+    public string Customer { get; set; }
+    public int Amount { get; set; }
+    public string Name { get; set; }
+    public IExportService? ExportService { get; set; }
+    
+    public Order(string customer, int amount, string name)
+    {
+        Customer = customer;
+        Amount = amount;
+        Name = name;
+    }
+    
+    public void Export()
+    {
+        ExportService?.Export(this);
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,19 @@
+﻿using Strategy;
+
+var order = new Order("Jane Doe", 100, "Order1")
+{
+    ExportService = new XmlExportService()
+};
+order.Export();
+
+var order2 = new Order("Misil", 200, "Order2")
+{
+    ExportService = new JsonExportService()
+};
+order2.Export();
+
+var order3 = new Order("Alicia", 300, "Order3")
+{
+    ExportService = new CsvExportService()
+};
+order3.Export();
