c3dc12a4fdb2d0bf368fcf4c5769c636b243c13a
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-08-01 23:07:44 -0600

    add template method pattern

---
+++ TemplateMethod/Implementation.cs
@@ -0,0 +1,58 @@
+﻿namespace TemplateMethod;
+
+/// <summary>
+/// Abstract class defining the template method pattern for parsing mail.
+/// </summary>
+public abstract class MailParser
+{
+    protected virtual void FindServer()
+    {
+        Console.WriteLine("Finding server...");
+    }
+
+    protected abstract void AuthenticateToServer();
+    
+    private string ParseHtmlMailBody(string identifier)
+    {
+        Console.WriteLine("Parsing HTML mail body...");
+        return $"Parsed HTML content for {identifier}";
+    }
+
+    public string ParseMailBody(string identifier)
+    {
+        Console.WriteLine("Parsing mail body... (in template method)");
+        FindServer();
+        AuthenticateToServer();
+        return ParseHtmlMailBody(identifier);
+    }
+}
+
+public class ExchangeMailParser : MailParser
+{
+    protected override void AuthenticateToServer()
+    {
+        Console.WriteLine("Authenticating to Exchange server...");
+    }
+}
+
+public class ApacheMailServer : MailParser
+{
+    protected override void AuthenticateToServer()
+    {
+        Console.WriteLine("Authenticating to Apache server...");
+    }
+}
+
+public class EudoraMailServer : MailParser
+{
+    protected override void FindServer()
+    {
+        Console.WriteLine("Finding Eudora server...");
+    }
+
+    protected override void AuthenticateToServer()
+    {
+        Console.WriteLine("Authenticating to Eudora server...");
+    }
+}
+
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using TemplateMethod;
+
+var exchangeParser = new ExchangeMailParser();
+Console.WriteLine(exchangeParser.ParseMailBody(Guid.NewGuid().ToString()));
+Console.WriteLine();
+
+var apacheParser = new ApacheMailServer();
+Console.WriteLine(apacheParser.ParseMailBody(Guid.NewGuid().ToString()));
+Console.WriteLine();
+
+var eudoraParser = new EudoraMailServer();
+Console.WriteLine(eudoraParser.ParseMailBody(Guid.NewGuid().ToString()));
+Console.WriteLine();
