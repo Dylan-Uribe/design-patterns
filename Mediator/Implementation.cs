7945fa8aa54d07fff867e21259bc879d6b728da1
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-08-03 22:45:08 -0600

    add first part of mediator pattern

---
+++ Mediator/Implementation.cs
@@ -0,0 +1,73 @@
+﻿namespace Mediator;
+
+/// <summary>
+/// Mediator interface for a chat room.
+/// </summary>
+public interface IChatRoom
+{
+    public void Register(TeamMember teamMember);
+    public void Send(string from, string message);
+}
+
+/// <summary>
+/// Colleague
+/// </summary>
+public abstract class TeamMember(string name)
+{
+    private IChatRoom? _chatRoom;
+
+    public string Name { get;} = name;
+
+    public void SetChatRoom(IChatRoom chatRoom)
+    {
+        _chatRoom = chatRoom;
+    }
+
+    public void Send(string message)
+    {
+        _chatRoom?.Send(Name, message);
+    }
+
+    public virtual void Receive(string from, string message)
+    {
+        Console.WriteLine($"message {from} to {Name}: {message}");
+    }
+}
+
+public class Lawyer(string name) : TeamMember(name)
+{
+    public override void Receive(string from, string message)
+    {
+        Console.WriteLine($"{nameof(Lawyer)} {Name} received: ");
+        base.Receive(from, message);
+    }
+}
+
+public class AccountManager(string name) : TeamMember(name)
+{
+    public override void Receive(string from, string message)
+    {
+        Console.WriteLine($"{nameof(AccountManager)} {Name} received: ");
+        base.Receive(from, message);
+    }
+}
+
+public class TeamChatRoom : IChatRoom
+{
+    private readonly Dictionary<string, TeamMember> _teamMembers = new();
+
+    public void Register(TeamMember teamMember)
+    {
+        teamMember.SetChatRoom(this);
+        _teamMembers.TryAdd(teamMember.Name, teamMember);
+    }
+
+    public void Send(string from, string message)
+    {
+        foreach (var teamMember in _teamMembers.Values)
+        {
+            if (teamMember.Name == from) continue;
+            teamMember.Receive(from, message);
+        }
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,12 @@
+﻿using Mediator;
+
+TeamChatRoom teamChatRoom = new();
+var steven = new Lawyer("Steven");
+var kenneth = new Lawyer("kenneth");
+var john = new AccountManager("John");
+
+teamChatRoom.Register(steven);
+teamChatRoom.Register(kenneth);
+teamChatRoom.Register(john);
+steven.Send("Hello everyone!");
+teamChatRoom.Send("kenneth", "Hello Steven!");
